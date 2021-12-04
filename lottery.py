06496fc16e8549f72c3c3e25a6c3a5ae911e963f
unknown <angelaescariocorpuz@gmail.com>
2021-12-04 17:40:25 +0800

    added in function for the program intro and another function where it asks the user the enter three numbers

--- lottery.py
+++ lottery.py
@@ -6,3 +6,24 @@
 #Display ”Try again y/n” after each game
 #If the user enter “y” the user will play again
 #if “n” the program will exit.
+
+import random 
+import time
+from colorama import Fore, Back, Style
+
+def programintro ():
+    print (Fore.YELLOW + "Welcome to loterry!")
+    time.sleep (2)
+    print (Fore.RESET)
+    print ("Please enter three numbers ranging from 0 to 9 and test your luck if the program will generate the same set of numbers as yours.")
+    time.sleep (2)
+    print ("Good luck!")
+
+def getnumbers ():
+    time.sleep (2)
+    threenumbers = int(input("Enter three numbers here: "))
+    return threenumbers
+
+programintro ()
+
+numbers = getnumbers ()
